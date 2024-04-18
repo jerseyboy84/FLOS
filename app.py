0b4cd3bacd2267815d293c3eb2198bdd53a0d7e0
Brian Rockermann <brian.rockermann@gmail.com>
2024-04-18 10:12:17 -0700

    revert to original python config with collections.abc.callable added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, abort, jsonify
 from models import setup_db, Student, Instructor
 from flask_cors import CORS
 from auth import AuthError, requires_auth
+import collections
+collections.Callable = collections.abc.Callable
 
 
 def create_app(test_config=None):
